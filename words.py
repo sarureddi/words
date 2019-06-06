8cbf7e0e2f172d18401a9e8df275a1e58bbc5cae
saru <sarumathiayyappan@gmail.com>
2019-06-06 10:52:11 +0530

    Create words.py

---
+++ words.py
@@ -0,0 +1,5 @@
+# words
+test_string="i love to code in all language"
+print ("The original string is : " +  test_string) 
+res = len(test_string.split()) 
+print ("The number of words in string are : " +  str(res)) 
